f57c9ec9cf216271655640ad060d53bc16e5570a
studytap-developer <studytapdevloper@gmail.com>
2025-09-12 12:47:46 +0530

    update

--- src/App.js
+++ src/App.js
@@ -62,29 +62,29 @@ import About from './components/About';
 import Footer from './components/Footer';
 import Visit from './components/Visit';
 import PrivacyPolicy from './components/PrivacyPolicy';
-import Chat from './components/chat';
+// import Chat from './components/chat';
 import WhatsAppPage from './components/WhatsAppPage';
-import FullScreenPopup from './components/FullScreenPopup';
+// import FullScreenPopup from './components/FullScreenPopup';
 
 function App() {
   const [showPopup, setShowPopup] = useState(false);
 
-  useEffect(() => {
-    // REMOVE THIS LINE AFTER TESTING IF YOU WANT POPUP ONLY ONCE
-    localStorage.removeItem('popupShown');
+  // useEffect(() => {
+  //   // REMOVE THIS LINE AFTER TESTING IF YOU WANT POPUP ONLY ONCE
+  //   localStorage.removeItem('popupShown');
 
-    const shown = localStorage.getItem('popupShown');
-    if (!shown) {
-      setShowPopup(true);
-      localStorage.setItem('popupShown', 'true');
-    }
-  }, []);
+  //   const shown = localStorage.getItem('popupShown');
+  //   if (!shown) {
+  //     setShowPopup(true);
+  //     localStorage.setItem('popupShown', 'true');
+  //   }
+  // }, []);
 
   return (
     <Router>
       <div className="App relative">
         {/* Show popup */}
-        {showPopup && <FullScreenPopup onClose={() => setShowPopup(false)} />}
+        {/* {showPopup && <FullScreenPopup onClose={() => setShowPopup(false)} />} */}
 
         {/* Hide navbar and chat when popup is open */}
         {!showPopup && <Navbar />}
@@ -107,7 +107,7 @@ function App() {
 
         <WhatsAppPage />
         <Footer />
-        {!showPopup && <Chat />}
+        {/* {!showPopup && <Chat />} */}
       </div>
     </Router>
   );
